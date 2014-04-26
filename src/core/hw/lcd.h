25d595d86912b079ab4d013cc2c0bd078a7276b3
bunnei <ericbunnie@gmail.com>
2014-04-26 01:32:05 -0400

    added (fake) physical addresses for where to put framebuffer in VRAM

--- src/core/hw/lcd.h
+++ src/core/hw/lcd.h
@@ -16,6 +16,7 @@ enum {
     TOP_WIDTH           = 400,
     BOTTOM_WIDTH        = 320,
 
+    // Physical addresses in FCRAM used by ARM9 applications - these are correct for real hardware 
     FRAMEBUFFER_SEL     = 0x20184E59,
     TOP_LEFT_FRAME1     = 0x20184E60,
     TOP_LEFT_FRAME2     = 0x201CB370,
@@ -23,6 +24,15 @@ enum {
     TOP_RIGHT_FRAME2    = 0x202C8670,
     SUB_FRAME1          = 0x202118E0,
     SUB_FRAME2          = 0x20249CF0,
+
+    // Physical addresses in VRAM - I'm not sure how these are actually allocated (so not real)
+    VRAM_FRAMEBUFFER_SEL    = 0x18184E59,
+    VRAM_TOP_LEFT_FRAME1    = 0x18184E60,
+    VRAM_TOP_LEFT_FRAME2    = 0x181CB370,
+    VRAM_TOP_RIGHT_FRAME1   = 0x18282160,
+    VRAM_TOP_RIGHT_FRAME2   = 0x182C8670,
+    VRAM_SUB_FRAME1         = 0x182118E0,
+    VRAM_SUB_FRAME2         = 0x18249CF0,
 };
 
 template <typename T>
