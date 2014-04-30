52377cf0d2e29143717898e82f09349d417da1a0
archshift <admin@archshift.com>
2014-04-29 19:27:01 -0700

    Some more experimentation

--- src/common/common.h
+++ src/common/common.h
@@ -21,11 +21,11 @@
 
 #define STACKALIGN
 
-#if __cplusplus >= 201103 || defined(_MSC_VER) || defined(__GXX_EXPERIMENTAL_CXX0X__)
+#if __cplusplus >= 201103L || defined(_MSC_VER) || defined(__GXX_EXPERIMENTAL_CXX0X__)
 #define HAVE_CXX11_SYNTAX 1
 #endif
 
-#if HAVE_CXX11_SYNTAX
+//#if HAVE_CXX11_SYNTAX
 // An inheritable class to disallow the copy constructor and operator= functions
 class NonCopyable
 {
@@ -37,7 +37,7 @@ private:
     NonCopyable(NonCopyable&);
     NonCopyable& operator=(NonCopyable& other);
 };
-#endif
+//#endif
 
 #include "common/log.h"
 #include "common/common_types.h"
