c1e71ae1ac6700e2e849c59e55baeacda7a20ae6
bunnei <ericbunnie@gmail.com>
2014-05-01 19:21:04 -0400

    fixed include in coprocessor.cpp

--- src/core/hle/coprocessor.cpp
+++ src/core/hle/coprocessor.cpp
@@ -2,7 +2,7 @@
 // Licensed under GPLv2
 // Refer to the license.txt file included.  
 
-#include "core/hle/mrc.h"
+#include "core/hle/coprocessor.h"
 #include "core/hle/hle.h"
 #include "core/mem_map.h"
 #include "core/core.h"
