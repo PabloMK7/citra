fbc04ad0c7c2bafde1b7b50957799c7bbab8f77b
bunnei <ericbunnie@gmail.com>
2014-05-17 13:48:27 -0400

    ignore thumbemu 0xDEADCODE debugging catch on MCR

--- src/core/arm/interpreter/armemu.cpp
+++ src/core/arm/interpreter/armemu.cpp
@@ -4478,8 +4478,7 @@ ARMul_Emulate26 (ARMul_State * state)
                                  isize) &
                                 R15PCBITS));
 #endif
-                    }
-                    else
+                    } else if (instr != 0xDEADC0DE) // thumbemu uses 0xDEADCODE for debugging to catch non updates 
                         ARMul_MCR (state, instr,
                                DEST);
                 }
