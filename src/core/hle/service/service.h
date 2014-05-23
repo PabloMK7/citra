0aa582bf89c3e3e479540b706511590636870912
bunnei <ericbunnie@gmail.com>
2014-05-22 23:48:37 -0400

    service: fixed typo that MSVC did not catch as an error

--- src/core/hle/service/service.h
+++ src/core/hle/service/service.h
@@ -71,7 +71,7 @@ public:
     /// Frees a handle from the service
     template <class T>
     void DeleteHandle(const Handle handle) {
-        g_object_pool.Destroy<T>(handle);
+        Kernel::g_object_pool.Destroy<T>(handle);
         m_handles.erase(std::remove(m_handles.begin(), m_handles.end(), handle), m_handles.end());
     }
 
