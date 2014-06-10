8957622d10784d5f04571e9ae01dbae13ed64c3e
bunnei <ericbunnie@gmail.com>
2014-06-09 22:30:39 -0400

    SVC: Renamed all function wrapper templates to Wrap, moved to HLE namespace.

--- src/core/hle/function_wrappers.h
+++ src/core/hle/function_wrappers.h
@@ -8,29 +8,27 @@
 #include "core/mem_map.h"
 #include "core/hle/hle.h"
 
-namespace Wrap {
+namespace HLE {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Function wrappers that return type s32
 
-namespace S32 {
-
-template<s32 func(u32, u32, u32, u32)> void U32_U32_U32_U32() {
+template<s32 func(u32, u32, u32, u32)> void Wrap() {
     RETURN(func(PARAM(0), PARAM(1), PARAM(2), PARAM(3)));
 }
 
-template<s32 func(u32, u32, u32, u32, u32)> void U32_U32_U32_U32_U32() {
+template<s32 func(u32, u32, u32, u32, u32)> void Wrap() {
     RETURN(func(PARAM(0), PARAM(1), PARAM(2), PARAM(3), PARAM(4)));
 }
 
-template<s32 func(u32*, u32, u32, u32, u32, u32)> void U32P_U32_U32_U32_U32_U32(){
+template<s32 func(u32*, u32, u32, u32, u32, u32)> void Wrap(){
     u32 param_1 = 0;
     u32 retval = func(&param_1, PARAM(0), PARAM(1), PARAM(2), PARAM(3), PARAM(4));
     Core::g_app_core->SetReg(1, param_1);
     RETURN(retval);
 }
 
-template<s32 func(s32*, u32*, s32, bool, s64)> void S32P_U32P_S32_Bool_S64() {
+template<s32 func(s32*, u32*, s32, bool, s64)> void Wrap() {
     s32 param_1 = 0;
     s32 retval = func(&param_1, (Handle*)Memory::GetPointer(PARAM(1)), (s32)PARAM(2), 
         (PARAM(3) != 0), (((s64)PARAM(4) << 32) | PARAM(0)));
@@ -39,82 +37,72 @@ template<s32 func(s32*, u32*, s32, bool, s64)> void S32P_U32P_S32_Bool_S64() {
 }
 
 // TODO(bunnei): Is this correct? Probably not
-template<s32 func(u32, u32, u32, u32, s64)> void U32_U32_U32_U32_S64() {
+template<s32 func(u32, u32, u32, u32, s64)> void Wrap() {
     RETURN(func(PARAM(5), PARAM(1), PARAM(2), PARAM(3), (((s64)PARAM(4) << 32) | PARAM(0))));
 }
 
-template<s32 func(u32, s64)> void U32_S64() {
+template<s32 func(u32, s64)> void Wrap() {
     RETURN(func(PARAM(0), (((s64)PARAM(3) << 32) | PARAM(2))));
 }
 
-template<s32 func(void*, void*, u32)> void VoidP_VoidP_U32(){
+template<s32 func(void*, void*, u32)> void Wrap(){
     RETURN(func(Memory::GetPointer(PARAM(0)), Memory::GetPointer(PARAM(1)), PARAM(2)));
 }
 
-template<s32 func(s32*, u32)> void S32P_U32(){
+template<s32 func(s32*, u32)> void Wrap(){
     s32 param_1 = 0;
     u32 retval = func(&param_1, PARAM(1));
     Core::g_app_core->SetReg(1, param_1);
     RETURN(retval);
 }
 
-template<s32 func(u32, s32)> void U32_S32() {
+template<s32 func(u32, s32)> void Wrap() {
     RETURN(func(PARAM(0), (s32)PARAM(1)));
 }
 
-template<s32 func(u32*, u32)> void U32P_U32(){
+template<s32 func(u32*, u32)> void Wrap(){
     u32 param_1 = 0;
     u32 retval = func(&param_1, PARAM(1));
     Core::g_app_core->SetReg(1, param_1);
     RETURN(retval);
 }
 
-template<s32 func(u32)> void U32() {
+template<s32 func(u32)> void Wrap() {
     RETURN(func(PARAM(0)));
 }
 
-template<s32 func(void*)> void U32P() {
+template<s32 func(void*)> void Wrap() {
     RETURN(func(Memory::GetPointer(PARAM(0))));
 }
 
-template<s32 func(s64*, u32, void*, s32)> void S64P_U32_VoidP_S32(){
+template<s32 func(s64*, u32, void*, s32)> void Wrap(){
     RETURN(func((s64*)Memory::GetPointer(PARAM(0)), PARAM(1), Memory::GetPointer(PARAM(2)), 
         (s32)PARAM(3)));
 }
 
-template<s32 func(u32*, const char*)> void U32P_CharP() {
+template<s32 func(u32*, const char*)> void Wrap() {
     u32 param_1 = 0;
     u32 retval = func(&param_1, Memory::GetCharPointer(PARAM(1)));
     Core::g_app_core->SetReg(1, param_1);
     RETURN(retval);
 }
 
-} // namespace S32
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Function wrappers that return type u32
 
-namespace U32 {
-
-template<u32 func()> void Void() {
+template<u32 func()> void Wrap() {
     RETURN(func());
 }
-
-} // namespace U32
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Function wrappers that return type void
 
-namespace Void {
-
-template<void func(s64)> void S64() {
+template<void func(s64)> void Wrap() {
     func(((s64)PARAM(1) << 32) | PARAM(0));
 }
 
-template<void func(const char*)> void CharP() {
+template<void func(const char*)> void Wrap() {
     func(Memory::GetCharPointer(PARAM(0)));
 }
 
-} // namespace Void
-
-} // namespace Wrap
+} // namespace HLE
