48e39fc9928f3dabc1954c1acb650d2f57f6a491
bunnei <ericbunnie@gmail.com>
2014-06-26 17:58:44 -0400

    FS: Added stubbed code to intercept and decode file system service functions.
    FS: Added to CMakeLists.txt

--- src/core/hle/service/service.cpp
+++ src/core/hle/service/service.cpp
@@ -10,6 +10,7 @@
 
 #include "core/hle/service/service.h"
 #include "core/hle/service/apt.h"
+#include "core/hle/service/fs.h"
 #include "core/hle/service/gsp.h"
 #include "core/hle/service/hid.h"
 #include "core/hle/service/ndm.h"
@@ -71,6 +72,7 @@ void Init() {
     
     g_manager->AddService(new SRV::Interface);
     g_manager->AddService(new APT_U::Interface);
+    g_manager->AddService(new FS_User::Interface);
     g_manager->AddService(new GSP_GPU::Interface);
     g_manager->AddService(new HID_User::Interface);
     g_manager->AddService(new NDM_U::Interface);
