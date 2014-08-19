90f23020f53c94dab800f7348877aae483974512
Sacha <xsacha@gmail.com>
2014-08-19 21:34:00 +1000

    Add Qt5 option. Use Qt5 by default.

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,7 +5,7 @@ set -e
 #if OS is linux or is not set
 if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then 
     mkdir build && cd build
-    cmake .. 
+    cmake -DUSE_QT5=OFF .. 
     make -j4
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
     mkdir build && cd build
--- externals/qhexedit/qhexedit_p.cpp
+++ externals/qhexedit/qhexedit_p.cpp
@@ -1,5 +1,3 @@
-#include <QtGui>
-
 #include "qhexedit_p.h"
 #include "commands.h"
 
@@ -437,7 +435,7 @@ void QHexEditPrivate::keyPressEvent(QKeyEvent *event)
 if (!_readOnly)
 {
     /* Hex input */
-        int key = int(event->text()[0].toAscii());
+        int key = int(event->text()[0].toLatin1());
         if ((key>='0' && key<='9') || (key>='a' && key <= 'f'))
         {
             if (getSelectionBegin() != getSelectionEnd())
