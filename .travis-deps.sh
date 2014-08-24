45976da975a21359cfcc9a05c575ed8b07575612
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-08-23 22:22:05 -0300

    CMake cleanup
    Several cleanups to the buildsystem:
     - Do better factoring of common libs between platforms.
     - Add support to building on Windows.
     - Remove Qt4 support.
     - Re-sort file lists and add missing headers.

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -8,10 +8,16 @@ if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     sudo apt-get -qq update
     sudo apt-get -qq install g++-4.8 xorg-dev libglu1-mesa-dev libxcursor-dev
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
-    git clone https://github.com/glfw/glfw.git
-    mkdir glfw/build && cd glfw/build
-    cmake .. && make && sudo make install 
-    cd -	
+    (
+        git clone https://github.com/glfw/glfw.git --branch 3.0.4 --depth 1
+        mkdir glfw/build && cd glfw/build
+        cmake .. && make -j2 && sudo make install
+    )
+
+    sudo apt-get install lib32stdc++6
+    sudo mkdir -p /usr/local
+    curl http://www.cmake.org/files/v2.8/cmake-2.8.11-Linux-i386.tar.gz \
+        | sudo tar -xz -C /usr/local --strip-components=1
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
     brew tap homebrew/versions
     brew install qt5 glfw3 pkgconfig
