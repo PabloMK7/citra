b5d9f7364e18aa2f596f6c8a466b1544a6c33a59
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-12-15 21:55:28 -0200

    Travis: Use gcc 4.9 instead of 4.8 since it's getting installed anyway

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -7,8 +7,8 @@ set -x
 if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get -qq update
-    sudo apt-get -qq install g++-4.8 xorg-dev libglu1-mesa-dev libxcursor-dev
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+    sudo apt-get -qq install g++-4.9 xorg-dev libglu1-mesa-dev libxcursor-dev
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
     (
         git clone https://github.com/glfw/glfw.git --branch 3.0.4 --depth 1
         mkdir glfw/build && cd glfw/build
