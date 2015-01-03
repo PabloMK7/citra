22cfa55302dbefdd3099522ae250c0d5723c2869
Subv <subv2112@gmail.com>
2015-01-02 21:39:31 -0500

    IVFCArchive: Use a critical log to notify of invalid operations.

--- src/core/file_sys/ivfc_archive.cpp
+++ src/core/file_sys/ivfc_archive.cpp
@@ -23,33 +23,33 @@ std::unique_ptr<FileBackend> IVFCArchive::OpenFile(const Path& path, const Mode 
 }
 
 bool IVFCArchive::DeleteFile(const Path& path) const {
-    LOG_WARNING(Service_FS, "Attempted to delete a file from an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to delete a file from an IVFC archive (%s).", GetName().c_str());
     return false;
 }
 
 bool IVFCArchive::RenameFile(const Path& src_path, const Path& dest_path) const {
-    LOG_WARNING(Service_FS, "Attempted to rename a file within an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to rename a file within an IVFC archive (%s).", GetName().c_str());
     return false;
 }
 
 bool IVFCArchive::DeleteDirectory(const Path& path) const {
-    LOG_WARNING(Service_FS, "Attempted to delete a directory from an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to delete a directory from an IVFC archive (%s).", GetName().c_str());
     return false;
 }
 
 ResultCode IVFCArchive::CreateFile(const Path& path, u32 size) const {
-    LOG_WARNING(Service_FS, "Attempted to create a file in an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to create a file in an IVFC archive (%s).", GetName().c_str());
     // TODO: Verify error code
     return ResultCode(ErrorDescription::NotAuthorized, ErrorModule::FS, ErrorSummary::NotSupported, ErrorLevel::Permanent);
 }
 
 bool IVFCArchive::CreateDirectory(const Path& path) const {
-    LOG_WARNING(Service_FS, "Attempted to create a directory in an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to create a directory in an IVFC archive (%s).", GetName().c_str());
     return false;
 }
 
 bool IVFCArchive::RenameDirectory(const Path& src_path, const Path& dest_path) const {
-    LOG_WARNING(Service_FS, "Attempted to rename a file within an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to rename a file within an IVFC archive (%s).", GetName().c_str());
     return false;
 }
 
@@ -58,7 +58,7 @@ std::unique_ptr<DirectoryBackend> IVFCArchive::OpenDirectory(const Path& path) c
 }
 
 ResultCode IVFCArchive::Format(const Path& path) const {
-    LOG_WARNING(Service_FS, "Attempted to format an IVFC archive (%s).", GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to format an IVFC archive (%s).", GetName().c_str());
     // TODO: Verify error code
     return ResultCode(ErrorDescription::NotAuthorized, ErrorModule::FS, ErrorSummary::NotSupported, ErrorLevel::Permanent);
 }
@@ -72,7 +72,7 @@ size_t IVFCFile::Read(const u64 offset, const u32 length, u8* buffer) const {
 }
 
 size_t IVFCFile::Write(const u64 offset, const u32 length, const u32 flush, const u8* buffer) const {
-    LOG_WARNING(Service_FS, "Attempted to write to IVFC file in archive %s.", archive->GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to write to IVFC file in archive %s.", archive->GetName().c_str());
     return 0;
 }
 
@@ -81,7 +81,7 @@ size_t IVFCFile::GetSize() const {
 }
 
 bool IVFCFile::SetSize(const u64 size) const {
-    LOG_WARNING(Service_FS, "Attempted to set the size of an IVFC file in archive %s", archive->GetName().c_str());
+    LOG_CRITICAL(Service_FS, "Attempted to set the size of an IVFC file in archive %s", archive->GetName().c_str());
     return false;
 }
 
