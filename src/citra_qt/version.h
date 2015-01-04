6dfcf2271b1ae0f6ea42994ac80d2df47e83e759
chrisvj <chrisvanderjagt@gmail.com>
2015-01-04 09:36:57 -0800

    citra-qt: Added license headers to files.

--- src/citra_qt/version.h
+++ src/citra_qt/version.h
@@ -1,3 +1,7 @@
+// Copyright 2014 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
 // TODO: Supposed to be generated...
 // GENERATED - Do not edit!
 #ifndef VERSION_H_
