a4a1ce766d9ac0284e4bb28a1c3f7f0f3d1a6204
Chin <chin.bimbo@gmail.com>
2015-01-24 10:45:06 -0500

    AppVeyor: Sanitize the file name before uploading

--- upload_to_mega.js
+++ upload_to_mega.js
@@ -1,11 +1,13 @@
 var util = require('util');
 var exec = require('child_process').exec;
+var sanitize = require("sanitize-filename");
 
-var email = 'chin.bimbo@gmail.com';
+var email = process.env.MEGA_EMAIL;
 var password = process.env.MEGA_PASSWORD;
 var sourceFileName = 'build.7z';
 var dstFileName = process.env.APPVEYOR_REPO_COMMIT.substring(0, 8) + " - " + 
                 process.env.APPVEYOR_REPO_COMMIT_MESSAGE.substring(0, 100) + ".7z";
+dstFileName = sanitize(dstFileName);
 
 var cmd = util.format('megaput ../%s --path \"/Root/Citra/Windows/%s\" --username=%s --password=%s --no-progress',
                         sourceFileName,
@@ -13,6 +15,7 @@ var cmd = util.format('megaput ../%s --path \"/Root/Citra/Windows/%s\" --usernam
                         email,
                         password);
 
+// only upload build on master branch, and not on other branches or PRs
 if (process.env.APPVEYOR_REPO_BRANCH == "master") {
     console.log("Uploading file " + dstFileName + " to Mega...");
     exec(cmd, function(error, stdout, stderr) {
