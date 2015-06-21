1775adc34c588c8c37ef8c07dc0c3ff2358d5a31
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2015-06-21 13:52:08 +0100

    Common: Remove unused ROUND_UP_POW2 macro.

--- src/common/common_funcs.h
+++ src/common/common_funcs.h
@@ -6,13 +6,6 @@
 
 #include "common_types.h"
 
-#define b2(x)   (   (x) | (   (x) >> 1) )
-#define b4(x)   ( b2(x) | ( b2(x) >> 2) )
-#define b8(x)   ( b4(x) | ( b4(x) >> 4) )
-#define b16(x)  ( b8(x) | ( b8(x) >> 8) )
-#define b32(x)  (b16(x) | (b16(x) >>16) )
-#define ROUND_UP_POW2(x)    (b32(x - 1) + 1)
-
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 /// Textually concatenates two tokens. The double-expansion is required by the C preprocessor.
