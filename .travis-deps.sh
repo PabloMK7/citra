a60f2a93d24237a44b3ca5bd113c4f5283de8384
Yuri Kunde Schlesner <yuriks@yuriks.net>
2015-07-10 20:04:28 -0300

    Travis: Update GLFW on Linux to 3.1.1

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -7,10 +7,13 @@ set -x
 if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
     export CC=gcc-4.9
     export CXX=g++-4.9
-
     mkdir -p $HOME/.local
+
+    curl http://www.cmake.org/files/v2.8/cmake-2.8.11-Linux-i386.tar.gz \
+        | tar -xz -C $HOME/.local --strip-components=1
+
     (
-        git clone https://github.com/glfw/glfw.git --branch 3.0.4 --depth 1
+        git clone https://github.com/glfw/glfw.git --branch 3.1.1 --depth 1
         mkdir glfw/build && cd glfw/build
         cmake -DBUILD_SHARED_LIBS=ON \
               -DGLFW_BUILD_EXAMPLES=OFF \
@@ -19,9 +22,6 @@ if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
               ..
         make -j4 && make install
     )
-
-    curl http://www.cmake.org/files/v2.8/cmake-2.8.11-Linux-i386.tar.gz \
-        | tar -xz -C $HOME/.local --strip-components=1
 
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     brew tap homebrew/versions
