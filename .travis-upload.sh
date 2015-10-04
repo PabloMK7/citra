addef06081c368928f1e76d929a8f176a6209eb2
archshift <gh@archshift.com>
2015-10-03 17:23:31 -0700

    OS X build uploading: auto-confirm SSH host key
    Prevents error verifying host key (and we can't manually verify since
    the prompt is obviously non-interactive).

--- .travis-upload.sh
+++ .travis-upload.sh
@@ -24,5 +24,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 
     ARCHIVE_NAME="${REV_NAME}.tar.xz"
     tar -cJvf "$ARCHIVE_NAME" "$REV_NAME"
-    lftp -c "open -u citra-builds,$BUILD_PASSWORD sftp://builds.citra-emu.org; put -O '$UPLOAD_DIR' '$ARCHIVE_NAME'"
+    lftp -c "open -u citra-builds,$BUILD_PASSWORD sftp://builds.citra-emu.org; set sftp:auto-confirm yes; put -O '$UPLOAD_DIR' '$ARCHIVE_NAME'"
 fi
