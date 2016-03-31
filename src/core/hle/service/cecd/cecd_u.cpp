cb8e85a37d0784689afc60fd208213a86181698a
mailwl <mailwl@gmail.com>
2016-03-31 12:21:02 +0300

    cecd:u: stub GetCecInfoEventHandle, GetChangeStateEventHandle

--- src/core/hle/service/cecd/cecd_u.cpp
+++ src/core/hle/service/cecd/cecd_u.cpp
@@ -2,13 +2,16 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include "core/hle/service/cecd/cecd.h"
 #include "core/hle/service/cecd/cecd_u.h"
 
 namespace Service {
 namespace CECD {
 
 static const Interface::FunctionInfo FunctionTable[] = {
-    { 0x00120104, nullptr, "ReadSavedData" },
+    {0x000F0000, GetCecInfoEventHandle,     "GetCecInfoEventHandle"},
+    {0x00100000, GetChangeStateEventHandle, "GetChangeStateEventHandle"},
+    {0x00120104, nullptr,                   "ReadSavedData"},
 };
 
 CECD_U_Interface::CECD_U_Interface() {
