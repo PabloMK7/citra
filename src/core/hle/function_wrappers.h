42a50da76b816fff95ae0e438d3e966f6f991b8a
Subv <subv2112@gmail.com>
2016-04-17 13:36:15 -0500

    Kernel/SVC: Fixed the register order for svcCreateMemoryBlock.
    R0 is used as the last parameter instead of R4.

--- src/core/hle/function_wrappers.h
+++ src/core/hle/function_wrappers.h
@@ -170,7 +170,8 @@ template<ResultCode func(s64*, u32, s32)> void Wrap() {
 
 template<ResultCode func(u32*, u32, u32, u32, u32)> void Wrap() {
     u32 param_1 = 0;
-    u32 retval = func(&param_1, PARAM(1), PARAM(2), PARAM(3), PARAM(4)).raw;
+    // The last parameter is passed in R0 instead of R4
+    u32 retval = func(&param_1, PARAM(1), PARAM(2), PARAM(3), PARAM(0)).raw;
     Core::g_app_core->SetReg(1, param_1);
     FuncReturn(retval);
 }
