f72bd5721240d9f33d803546c4bbc21a6f4ee466
Subv <subv2112@gmail.com>
2016-04-17 21:10:17 -0500

    Kernel/Memory: Remove the Shared Memory region from the legacy memory map.

--- src/core/hle/kernel/memory.cpp
+++ src/core/hle/kernel/memory.cpp
@@ -107,7 +107,6 @@ struct MemoryArea {
 
 // We don't declare the IO regions in here since its handled by other means.
 static MemoryArea memory_areas[] = {
-    {SHARED_MEMORY_VADDR, SHARED_MEMORY_SIZE,     "Shared Memory"}, // Shared memory
     {VRAM_VADDR,          VRAM_SIZE,              "VRAM"},          // Video memory (VRAM)
     {TLS_AREA_VADDR,      TLS_AREA_SIZE,          "TLS Area"},      // TLS memory
 };
