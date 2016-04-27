7d106eff1054204587d8c4f5537102640d1c7806
MerryMage <MerryMage@users.noreply.github.com>
2016-04-27 12:04:15 +0100

    AudioCore: Implement NullSink

---
+++ src/audio_core/null_sink.h
@@ -0,0 +1,29 @@
+// Copyright 2016 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <cstddef>
+
+#include "audio_core/audio_core.h"
+#include "audio_core/sink.h"
+
+namespace AudioCore {
+
+class NullSink final : public Sink {
+public:
+    ~NullSink() override = default;
+
+    unsigned int GetNativeSampleRate() const override {
+        return native_sample_rate;
+    }
+
+    void EnqueueSamples(const std::vector<s16>&) override {}
+
+    size_t SamplesInQueue() const override {
+        return 0;
+    }
+};
+
+} // namespace AudioCore
