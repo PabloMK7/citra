49bfe9bf91532d82cd082bf3316ce1bb0d24cc86
Jannik Vogel <email@jannikvogel.de>
2016-04-30 09:43:59 +0200

    Remove TGA dumper

--- src/video_core/utils.h
+++ src/video_core/utils.h
@@ -10,31 +10,6 @@
 
 namespace VideoCore {
 
-/// Structure for the TGA texture format (for dumping)
-struct TGAHeader {
-    char  idlength;
-    char  colormaptype;
-    char  datatypecode;
-    short int colormaporigin;
-    short int colormaplength;
-    short int x_origin;
-    short int y_origin;
-    short width;
-    short height;
-    char  bitsperpixel;
-    char  imagedescriptor;
-};
-
-/**
- * Dumps a texture to TGA
- * @param filename String filename to dump texture to
- * @param width Width of texture in pixels
- * @param height Height of texture in pixels
- * @param raw_data Raw RGBA8 texture data to dump
- * @todo This should be moved to some general purpose/common code
- */
-void DumpTGA(std::string filename, short width, short height, u8* raw_data);
-
 /**
  * Interleave the lower 3 bits of each coordinate to get the intra-block offsets, which are
  * arranged in a Z-order curve. More details on the bit manipulation at:
