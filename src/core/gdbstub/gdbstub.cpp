0aee903adab317a11209aed4f4cb176cf62ca6d9
Jannik Vogel <email@jannikvogel.de>
2016-12-05 15:09:14 +0100

    gdbstub: Remove unused include

--- src/core/gdbstub/gdbstub.cpp
+++ src/core/gdbstub/gdbstub.cpp
@@ -17,7 +17,6 @@
 #ifdef _WIN32
 #include <winsock2.h>
 // winsock2.h needs to be included first to prevent winsock.h being included by other includes
-#include <common/x64/abi.h>
 #include <io.h>
 #include <iphlpapi.h>
 #include <ws2tcpip.h>
