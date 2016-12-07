26ea6b954636b334e4f31da97c30e215aa83f4a1
Lioncash <mathew1800@gmail.com>
2016-12-07 17:42:51 -0500

    Update AM service function tables
    Updated based off information from 3dbrew.

--- src/core/hle/service/am/am_app.cpp
+++ src/core/hle/service/am/am_app.cpp
@@ -14,9 +14,14 @@ const Interface::FunctionInfo FunctionTable[] = {
     {0x10030142, ListContentInfos, "ListContentInfos"},
     {0x10040102, DeleteContents, "DeleteContents"},
     {0x10050084, GetDataTitleInfos, "GetDataTitleInfos"},
+    {0x10060080, nullptr, "GetNumDataTitleTickets"},
     {0x10070102, ListDataTitleTicketInfos, "ListDataTitleTicketInfos"},
+    {0x100801C2, nullptr, "GetItemRights"},
     {0x100900C0, nullptr, "IsDataTitleInUse"},
     {0x100A0000, nullptr, "IsExternalTitleDatabaseInitialized"},
+    {0x100B00C0, nullptr, "GetNumExistingContentInfos"},
+    {0x100C0142, nullptr, "ListExistingContentInfos"},
+    {0x100D0084, nullptr, "GetPatchTitleInfos"},
 };
 
 AM_APP_Interface::AM_APP_Interface() {
