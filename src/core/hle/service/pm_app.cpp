c67dae54b9156d0a0ad3ef5f027b2b66ed68459e
Lioncash <mathew1800@gmail.com>
2016-12-07 23:54:33 -0500

    pm_app: Update function table
    Updated based off information from 3dbrew.

--- src/core/hle/service/pm_app.cpp
+++ src/core/hle/service/pm_app.cpp
@@ -10,18 +10,21 @@
 namespace PM_APP {
 
 const Interface::FunctionInfo FunctionTable[] = {
+    // clang-format off
     {0x00010140, nullptr, "LaunchTitle"},
-    {0x00020082, nullptr, "LaunchFIRMSetParams"},
-    {0x00030080, nullptr, "TerminateProcesse"},
-    {0x00040100, nullptr, "TerminateProcessTID"},
-    {0x000500C0, nullptr, "TerminateProcessTID_unknown"},
+    {0x00020082, nullptr, "LaunchFIRM"},
+    {0x00030080, nullptr, "TerminateApplication"},
+    {0x00040100, nullptr, "TerminateTitle"},
+    {0x000500C0, nullptr, "TerminateProcess"},
+    {0x00060082, nullptr, "PrepareForReboot"},
     {0x00070042, nullptr, "GetFIRMLaunchParams"},
     {0x00080100, nullptr, "GetTitleExheaderFlags"},
     {0x00090042, nullptr, "SetFIRMLaunchParams"},
-    {0x000A0140, nullptr, "SetResourceLimit"},
-    {0x000B0140, nullptr, "GetResourceLimitMax"},
+    {0x000A0140, nullptr, "SetAppResourceLimit"},
+    {0x000B0140, nullptr, "GetAppResourceLimit"},
     {0x000C0080, nullptr, "UnregisterProcess"},
     {0x000D0240, nullptr, "LaunchTitleUpdate"},
+    // clang-format on
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
