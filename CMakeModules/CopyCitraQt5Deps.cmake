b7b2627b3746e28fe64c9af74583c8b24edee909
freiro <f73b2894@opayq.com>
2016-12-13 00:01:48 +0100

    Modularized Qt and SDL file copying Now cmake relies on two submodules to copy the libraries in the proper folders

--- CMakeModules/CopyCitraQt5Deps.cmake
+++ CMakeModules/CopyCitraQt5Deps.cmake
@@ -1,12 +1,10 @@
-function(copy_msvc_libraries)
+function(copy_citra_Qt5_deps target_dir)
     include(WindowsCopyFiles)
     set(DLL_DEST "${CMAKE_BINARY_DIR}/bin/$<CONFIG>/")
-    windows_copy_files(citra-qt ${SDL2_DLL_DIR} ${DLL_DEST} SDL2.dll)
-
     set(Qt5_DLL_DIR "${Qt5_DIR}/../../../bin")
     set(Qt5_PLATFORMS_DIR "${Qt5_DIR}/../../../plugins/platforms/")
     set(PLATFORMS ${DLL_DEST}platforms/)
-    windows_copy_files(citra-qt ${Qt5_DLL_DIR} ${DLL_DEST}
+    windows_copy_files(${target_dir} ${Qt5_DLL_DIR} ${DLL_DEST}
         icudt*.dll
         icuin*.dll
         icuuc*.dll
@@ -16,4 +14,4 @@ function(copy_msvc_libraries)
         Qt5Widgets$<$<CONFIG:Debug>:d>.*
     )
     windows_copy_files(citra-qt ${Qt5_PLATFORMS_DIR} ${PLATFORMS} qwindows$<$<CONFIG:Debug>:d>.*)
-endfunction(copy_msvc_libraries)
+endfunction(copy_citra_Qt5_deps)
