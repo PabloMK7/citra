85ba60d5ecc6fbd809223318d76b6e7fb3c07d27
wwylele <wwylele@gmail.com>
2017-02-27 22:43:10 +0200

    Timer: restore missing signaled=true from #2421

--- src/core/hle/kernel/timer.cpp
+++ src/core/hle/kernel/timer.cpp
@@ -80,6 +80,8 @@ void Timer::WakeupAllWaitingThreads() {
 void Timer::Signal(int cycles_late) {
     LOG_TRACE(Kernel, "Timer %u fired", GetObjectId());
 
+    signaled = true;
+
     // Resume all waiting threads
     WakeupAllWaitingThreads();
 
