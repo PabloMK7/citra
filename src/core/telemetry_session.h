120b00fb1aee96aec42b1647ece98dcc3e192139
bunnei <bunneidev@gmail.com>
2017-05-02 00:10:04 -0400

    telemetry: Log a few simple data fields throughout core.

--- src/core/telemetry_session.h
+++ src/core/telemetry_session.h
@@ -31,7 +31,7 @@ public:
     }
 
 private:
-    Telemetry::FieldCollection field_collection; ///< Field collection, tracks all added fields
+    Telemetry::FieldCollection field_collection; ///< Tracks all added fields for the session
     std::unique_ptr<Telemetry::VisitorInterface> backend; ///< Backend interface that logs fields
 };
 
