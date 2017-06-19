90b8d4dd367db6a69c32584c9a0e762de883706e
Yuri Kunde Schlesner <yuriks@yuriks.net>
2017-06-18 18:38:40 -0700

    Kernel: Add comment about the extended linear heap area

--- src/core/hle/kernel/process.cpp
+++ src/core/hle/kernel/process.cpp
@@ -151,6 +151,8 @@ void Process::Run(s32 main_thread_priority, u32 stack_size) {
 }
 
 VAddr Process::GetLinearHeapAreaAddress() const {
+    // Starting from system version 8.0.0 a new linear heap layout is supported to allow usage of
+    // the extra RAM in the n3DS.
     return kernel_version < 0x22C ? Memory::LINEAR_HEAP_VADDR : Memory::NEW_LINEAR_HEAP_VADDR;
 }
 
