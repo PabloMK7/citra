a634efa40e988d86ae21d86ab1a93e062614fd0b
bunnei <bunneidev@gmail.com>
2017-06-27 23:18:52 -0400

    web_backend: Add initial interface to POST data to Citra Web Services.

--- src/web_service/web_backend.cpp
+++ src/web_service/web_backend.cpp
@@ -2,8 +2,49 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <cpr/cpr.h>
+#include <stdlib.h>
+#include "common/logging/log.h"
 #include "web_service/web_backend.h"
 
 namespace WebService {
+
+static constexpr char ENV_VAR_USERNAME[]{"CITRA_WEB_SERVICES_USERNAME"};
+static constexpr char ENV_VAR_TOKEN[]{"CITRA_WEB_SERVICES_TOKEN"};
+
+static std::string GetEnvironmentVariable(const char* name) {
+    const char* value{getenv(name)};
+    if (value) {
+        return value;
+    }
+    return {};
+}
+
+const std::string& GetUsername() {
+    static const std::string username{GetEnvironmentVariable(ENV_VAR_USERNAME)};
+    return username;
+}
+
+const std::string& GetToken() {
+    static const std::string token{GetEnvironmentVariable(ENV_VAR_TOKEN)};
+    return token;
+}
+
+void PostJson(const std::string& url, const std::string& data) {
+    if (url.empty()) {
+        LOG_ERROR(WebService, "URL is invalid");
+        return;
+    }
+
+    if (GetUsername().empty() || GetToken().empty()) {
+        LOG_ERROR(WebService, "Environment variables %s and %s must be set to POST JSON",
+                  ENV_VAR_USERNAME, ENV_VAR_TOKEN);
+        return;
+    }
+
+    cpr::PostAsync(cpr::Url{url}, cpr::Body{data}, cpr::Header{{"Content-Type", "application/json"},
+                                                               {"x-username", GetUsername()},
+                                                               {"x-token", GetToken()}});
+}
 
 } // namespace WebService
--- src/web_service/web_backend.h
+++ src/web_service/web_backend.h
@@ -4,6 +4,28 @@
 
 #pragma once
 
+#include <string>
+#include "common/common_types.h"
+
 namespace WebService {
+
+/**
+ * Gets the current username for accessing services.citra-emu.org.
+ * @returns Username as a string, empty if not set.
+ */
+const std::string& GetUsername();
+
+/**
+ * Gets the current token for accessing services.citra-emu.org.
+ * @returns Token as a string, empty if not set.
+ */
+const std::string& GetToken();
+
+/**
+ * Posts JSON to services.citra-emu.org.
+ * @param url URL of the services.citra-emu.org endpoint to post data to.
+ * @param data String of JSON data to use for the body of the POST request.
+ */
+void PostJson(const std::string& url, const std::string& data);
 
 } // namespace WebService
