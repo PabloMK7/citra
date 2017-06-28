52fbe1e10cffcfd4c4ee1c50b7c8e98f3eb4cb50
bunnei <bunneidev@gmail.com>
2017-06-27 23:01:49 -0400

    web_service: Add skeleton project.

---
+++ src/web_service/web_backend.cpp
@@ -0,0 +1,9 @@
+// Copyright 2017 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include "web_service/web_backend.h"
+
+namespace WebService {
+
+} // namespace WebService
---
+++ src/web_service/web_backend.h
@@ -0,0 +1,9 @@
+// Copyright 2017 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+namespace WebService {
+
+} // namespace WebService
