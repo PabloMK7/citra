f5be3258f23d4f01a7501638b5dd8964c6a0d4cc
MerryMage <MerryMage@users.noreply.github.com>
2017-10-21 13:59:14 +0100

    hle/lock: Remove #pragma once in cpp file

--- src/core/hle/lock.cpp
+++ src/core/hle/lock.cpp
@@ -2,10 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#pragma once
-
 #include <core/hle/lock.h>
 
 namespace HLE {
 std::recursive_mutex g_hle_lock;
-}
+} // namespace HLE
