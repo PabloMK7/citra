cc2ff226ae189a24b0a6d4a3bd1e5e7748164e37
Lioncash <mathew1800@gmail.com>
2017-12-03 19:23:56 -0500

    vector_math: Return by const reference for const operator[]
    Makes behavior between both overloads consistent.

--- src/common/vector_math.h
+++ src/common/vector_math.h
@@ -131,7 +131,7 @@ public:
     {
         return *((&x) + i);
     }
-    T operator[](const int i) const {
+    const T& operator[](const int i) const {
         return *((&x) + i);
     }
 
@@ -288,7 +288,7 @@ public:
     {
         return *((&x) + i);
     }
-    T operator[](const int i) const {
+    const T& operator[](const int i) const {
         return *((&x) + i);
     }
 
@@ -502,7 +502,7 @@ public:
     {
         return *((&x) + i);
     }
-    T operator[](const int i) const {
+    const T& operator[](const int i) const {
         return *((&x) + i);
     }
 
