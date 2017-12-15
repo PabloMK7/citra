798a1c6ae97a2554175ca5e017cd14d8db0da831
wwylele <wwylele@gmail.com>
2017-12-15 10:07:31 +0200

    Travis, Appveyor: build with languages

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -6,7 +6,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.9
 export Qt5_DIR=$(brew --prefix)/opt/qt5
 
 mkdir build && cd build
-cmake .. -DUSE_SYSTEM_CURL=ON -DCMAKE_OSX_ARCHITECTURES="x86_64;x86_64h" -DCMAKE_BUILD_TYPE=Release
+cmake .. -DUSE_SYSTEM_CURL=ON -DCMAKE_OSX_ARCHITECTURES="x86_64;x86_64h" -DCMAKE_BUILD_TYPE=Release -DENABLE_QT_TRANSLATION=ON
 make -j4
 
 ctest -VV -C Release
