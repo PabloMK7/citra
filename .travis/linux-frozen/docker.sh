ce31cf531d85ad4a35cd0d71fecd13cd48ada890
MerryMage <MerryMage@users.noreply.github.com>
2018-02-06 12:25:39 +0000

    CI: Update to C++17 and use Ubuntu 18.04 as baseline

--- .travis/linux-frozen/docker.sh
+++ .travis/linux-frozen/docker.sh
@@ -9,16 +9,16 @@ apt-get install -y build-essential wget git python-launchpadlib libssl-dev
 # The apt repositories remove older versions regularly, so we can't use
 # apt-get and have to pull the packages directly from the archives.
 /citra/.travis/linux-frozen/install_package.py       \
-    libsdl2-dev 2.0.4+dfsg1-2ubuntu2 xenial          \
-    qtbase5-dev 5.2.1+dfsg-1ubuntu14.3 trusty        \
-    libqt5opengl5-dev 5.2.1+dfsg-1ubuntu14.3 trusty  \
-    libcurl4-openssl-dev 7.47.0-1ubuntu2.3 xenial    \
-    libicu52 52.1-3ubuntu0.6 trusty
+    libsdl2-dev 2.0.7+dfsg1-3ubuntu1 bionic          \
+    qtbase5-dev 5.9.3+dfsg-0ubuntu2 bionic           \
+    libqt5opengl5-dev 5.9.3+dfsg-0ubuntu2 bionic     \
+    libcurl4-openssl-dev 7.58.0-2ubuntu1 bionic      \
+    libicu57 57.1-6ubuntu0.2 bionic
 
 # Get a recent version of CMake
-wget https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.sh
-echo y | sh cmake-3.9.0-Linux-x86_64.sh --prefix=cmake
-export PATH=/citra/cmake/cmake-3.9.0-Linux-x86_64/bin:$PATH
+wget https://cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.sh
+echo y | sh cmake-3.10.1-Linux-x86_64.sh --prefix=cmake
+export PATH=/citra/cmake/cmake-3.10.1-Linux-x86_64/bin:$PATH
 
 mkdir build && cd build
 cmake .. -DUSE_SYSTEM_CURL=ON -DCMAKE_BUILD_TYPE=Release -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"}
