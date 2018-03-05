e004e9505ec232b96fe8520107d822b5919eec3c
wwylele <wwylele@gmail.com>
2018-03-05 14:50:49 +0200

    assert: do not crash on unimplemented code in debug build

--- src/common/assert.h
+++ src/common/assert.h
@@ -52,5 +52,5 @@ __declspec(noinline, noreturn)
 #define DEBUG_ASSERT_MSG(_a_, _desc_, ...)
 #endif
 
-#define UNIMPLEMENTED() DEBUG_ASSERT_MSG(false, "Unimplemented code!")
+#define UNIMPLEMENTED() LOG_CRITICAL(Debug, "Unimplemented code!")
 #define UNIMPLEMENTED_MSG(_a_, ...) ASSERT_MSG(false, _a_, __VA_ARGS__)
