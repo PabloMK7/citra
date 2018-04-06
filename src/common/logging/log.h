501717e9db34a2366eda0cf358fde2a5c4ed5c61
James Rowe <jroweboy@gmail.com>
2018-04-05 22:33:12 -0600

    Update fmtlib to fix msvc warnings
    Additionally, when updating fmtlib, there was a change in fmtlib broke
    how the old logging macro was overloaded, so this works around that by
    just naming the fmtlib macro impl something different

--- src/common/logging/log.h
+++ src/common/logging/log.h
@@ -112,13 +112,15 @@ void LogMessage(Class log_class, Level log_level, const char* filename, unsigned
     ;
 
 /// Logs a message to the global logger, using fmt
-void LogMessage(Class log_class, Level log_level, const char* filename, unsigned int line_num,
-                const char* function, const char* format, const fmt::format_args& args);
+void FmtLogMessageImpl(Class log_class, Level log_level, const char* filename,
+                       unsigned int line_num, const char* function, const char* format,
+                       const fmt::format_args& args);
 
 template <typename... Args>
 void FmtLogMessage(Class log_class, Level log_level, const char* filename, unsigned int line_num,
                    const char* function, const char* format, const Args&... args) {
-    LogMessage(log_class, log_level, filename, line_num, function, format, fmt::make_args(args...));
+    FmtLogMessageImpl(log_class, log_level, filename, line_num, function, format,
+                      fmt::make_args(args...));
 }
 
 } // namespace Log
