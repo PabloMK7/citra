16981d6be45a67a72b2a2ba631e45d68084d6913
liushuyu <liushuyu011@gmail.com>
2018-04-10 21:09:35 -0600

    travis: use cache

--- .travis/linux-frozen/build.sh
+++ .travis/linux-frozen/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
-
+mkdir "$HOME/.ccache" || true
 docker pull ubuntu:18.04
-docker run -e ENABLE_COMPATIBILITY_REPORTING -v $(pwd):/citra ubuntu:18.04 /bin/bash -ex /citra/.travis/linux-frozen/docker.sh
+docker run -e ENABLE_COMPATIBILITY_REPORTING -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache ubuntu:18.04  /bin/bash -ex /citra/.travis/linux-frozen/docker.sh
--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -4,6 +4,7 @@ set -o pipefail
 
 export MACOSX_DEPLOYMENT_TARGET=10.12
 export Qt5_DIR=$(brew --prefix)/opt/qt5
+export PATH="/usr/local/opt/ccache/libexec:$PATH"
 
 mkdir build && cd build
 cmake .. -DCMAKE_OSX_ARCHITECTURES="x86_64;x86_64h" -DCMAKE_BUILD_TYPE=Release -DENABLE_QT_TRANSLATION=ON -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"}
