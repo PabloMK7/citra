0d3983bc66c658b1d2a735e87056d5d43d7391e3
liushuyu <liushuyu011@gmail.com>
2018-04-13 20:30:04 -0600

    travis: use mkdir -p to avoid `|| true`

--- .travis/linux-frozen/build.sh
+++ .travis/linux-frozen/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
-mkdir "$HOME/.ccache" || true
+mkdir -p "$HOME/.ccache"
 docker pull ubuntu:18.04
 docker run -e ENABLE_COMPATIBILITY_REPORTING -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache ubuntu:18.04 /bin/bash -ex /citra/.travis/linux-frozen/docker.sh
