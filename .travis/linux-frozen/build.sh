613593167079ef874faa06095bfa06f39c7013a3
liushuyu <liushuyu011@gmail.com>
2018-06-08 02:01:36 -0600

    travis: share env variables with Docker

--- .travis/linux-frozen/build.sh
+++ .travis/linux-frozen/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 mkdir -p "$HOME/.ccache"
 docker pull ubuntu:18.04
-docker run -e ENABLE_COMPATIBILITY_REPORTING -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache ubuntu:18.04 /bin/bash -ex /citra/.travis/linux-frozen/docker.sh
+docker run --env-file .travis/common/travis-ci.env -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache ubuntu:18.04 /bin/bash -ex /citra/.travis/linux-frozen/docker.sh
