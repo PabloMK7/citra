ed18140daa4c5da42f5713cb0c118bf814c5d58a
NarcolepticK <NarcolepticKrias@gmail.com>
2018-06-23 14:12:54 -0400

    hle/kernel: Addressed comment about obscure formating

--- src/core/hle/kernel/svc.cpp
+++ src/core/hle/kernel/svc.cpp
@@ -663,9 +663,9 @@ static void Break(u8 break_reason) {
 
 /// Used to output a message on a debug hardware unit - does nothing on a retail unit
 static void OutputDebugString(VAddr address, int len) {
-    std::vector<char> string(len);
+    std::string string(len, ' ');
     Memory::ReadBlock(address, string.data(), len);
-    NGLOG_DEBUG(Debug_Emulated, "{:.{}}", string.data(), len);
+    NGLOG_DEBUG(Debug_Emulated, "{}", string);
 }
 
 /// Get resource limit
