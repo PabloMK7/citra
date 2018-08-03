384c9e5382b88bac75598d70b58aa2b6e8ae2035
Lioncash <mathew1800@gmail.com>
2018-08-02 21:51:08 -0400

    input_common: Use std::move where applicable
    Avoids unnecessary atomic reference count increments and decrements

--- src/input_common/keyboard.cpp
+++ src/input_common/keyboard.cpp
@@ -5,6 +5,7 @@
 #include <atomic>
 #include <list>
 #include <mutex>
+#include <utility>
 #include "input_common/keyboard.h"
 
 namespace InputCommon {
@@ -12,7 +13,7 @@ namespace InputCommon {
 class KeyButton final : public Input::ButtonDevice {
 public:
     explicit KeyButton(std::shared_ptr<KeyButtonList> key_button_list_)
-        : key_button_list(key_button_list_) {}
+        : key_button_list(std::move(key_button_list_)) {}
 
     ~KeyButton() override;
 
