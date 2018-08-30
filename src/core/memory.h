8fa861c2c66163ac8537e5afa8ca256582692520
Lioncash <Mathew1800@gmail.com>
2018-08-31 00:28:28 +0200

    memory: Make prototype parameter names match their definitions
    Keeps the code consistent.

--- src/core/memory.h
+++ src/core/memory.h
@@ -182,9 +182,9 @@ PageTable* GetCurrentPageTable();
 
 /// Determines if the given VAddr is valid for the specified process.
 bool IsValidVirtualAddress(const Kernel::Process& process, VAddr vaddr);
-bool IsValidVirtualAddress(VAddr addr);
+bool IsValidVirtualAddress(VAddr vaddr);
 
-bool IsValidPhysicalAddress(PAddr addr);
+bool IsValidPhysicalAddress(PAddr paddr);
 
 u8 Read8(VAddr addr);
 u16 Read16(VAddr addr);
@@ -206,9 +206,9 @@ void ZeroBlock(VAddr dest_addr, const size_t size);
 void CopyBlock(const Kernel::Process& process, VAddr dest_addr, VAddr src_addr, size_t size);
 void CopyBlock(VAddr dest_addr, VAddr src_addr, size_t size);
 
-u8* GetPointer(VAddr virtual_address);
+u8* GetPointer(VAddr vaddr);
 
-std::string ReadCString(VAddr virtual_address, std::size_t max_length);
+std::string ReadCString(VAddr vaddr, std::size_t max_length);
 
 /**
  * Converts a virtual address inside a region with 1:1 mapping to physical memory to a physical
@@ -227,7 +227,7 @@ PAddr VirtualToPhysicalAddress(VAddr addr);
 /**
  * Undoes a mapping performed by VirtualToPhysicalAddress().
  */
-boost::optional<VAddr> PhysicalToVirtualAddress(PAddr addr);
+boost::optional<VAddr> PhysicalToVirtualAddress(PAddr paddr);
 
 /**
  * Gets a pointer to the memory region beginning at the specified physical address.
