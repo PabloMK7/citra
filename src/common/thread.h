1855fb3d88c3487861e7beddb222dc07fbff7853
Weiyi Wang <wwylele@gmail.com>
2018-09-22 00:32:19 -0400

    common/thread: remove YieldCPU()
    simply use the standard library yield()

--- src/common/thread.h
+++ src/common/thread.h
@@ -96,14 +96,6 @@ private:
 
 void SleepCurrentThread(int ms);
 void SwitchCurrentThread(); // On Linux, this is equal to sleep 1ms
-
-// Use this function during a spin-wait to make the current thread
-// relax while another thread is working. This may be more efficient
-// than using events because event functions use kernel calls.
-inline void YieldCPU() {
-    std::this_thread::yield();
-}
-
 void SetCurrentThreadName(const char* name);
 
 } // namespace Common
