a74d97b15faf2c5f190fa745fbc66bb5366c8a91
Lioncash <mathew1800@gmail.com>
2018-10-25 16:52:21 -0400

    configure_system: Default initialize member variables
    These should be initialized to deterministic values so it's easier to
    catch improper behavior, as it'll always be reproducable, instead of
    performing uninitialized reads.

--- src/citra_qt/configuration/configure_system.h
+++ src/citra_qt/configuration/configure_system.h
@@ -38,13 +38,14 @@ private:
     void RefreshConsoleID();
 
     std::unique_ptr<Ui::ConfigureSystem> ui;
-    bool enabled;
+    bool enabled = false;
 
     std::shared_ptr<Service::CFG::Module> cfg;
     std::u16string username;
-    int birthmonth, birthday;
-    int language_index;
-    int sound_index;
+    int birthmonth = 0;
+    int birthday = 0;
+    int language_index = 0;
+    int sound_index = 0;
     u8 country_code;
     u16 play_coin;
 };
