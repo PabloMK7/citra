f903d3161dd0e25f656c54fe23a9cdfa6d5c2575
fearlessTobi <thm.frey@gmail.com>
2019-02-09 15:01:58 +0100

    ptm: move ptm_shared_extdata_id to header

--- src/core/hle/service/ptm/ptm.h
+++ src/core/hle/service/ptm/ptm.h
@@ -15,6 +15,9 @@ class System;
 
 namespace Service::PTM {
 
+/// Id of the SharedExtData archive used by the PTM process
+static const std::vector<u8> ptm_shared_extdata_id = {0, 0, 0, 0, 0x0B, 0, 0, 0xF0, 0, 0, 0, 0};
+
 /// Charge levels used by PTM functions
 enum class ChargeLevels : u32 {
     CriticalBattery = 1,
