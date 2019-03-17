a5c087f5de7eb7f6e0cce80d935ba41b48f5677b
Lioncash <mathew1800@gmail.com>
2019-03-17 04:02:48 -0400

    input_common/sdl_impl: Remove unused variable in SDLState constructor

--- src/input_common/sdl/sdl_impl.cpp
+++ src/input_common/sdl/sdl_impl.cpp
@@ -477,7 +477,6 @@ SDLState::SDLState() {
     if (start_thread) {
         poll_thread = std::thread([&] {
             using namespace std::chrono_literals;
-            SDL_Event event;
             while (initialized) {
                 SDL_PumpEvents();
                 std::this_thread::sleep_for(std::chrono::duration(10ms));
