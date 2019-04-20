2a44abf185a8d75cee1e373173b948e87ca44969
tkeph616 <tkeph616@students.bju.edu>
2019-04-20 00:40:26 -0400

    Launch directly in Mac without terminal

--- .travis/macos/upload.sh
+++ .travis/macos/upload.sh
@@ -20,18 +20,6 @@ $(brew --prefix)/opt/qt5/bin/macdeployqt "${REV_NAME}/citra-qt.app" -executable=
 # move libs into folder for deployment
 macpack "${REV_NAME}/citra" -d "libs"
 
-# Make the citra-qt.app application launch a debugging terminal.
-# Store away the actual binary
-mv ${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt ${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt-bin
-
-cat > ${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt <<EOL
-#!/usr/bin/env bash
-cd "\`dirname "\$0"\`"
-chmod +x citra-qt-bin
-open citra-qt-bin --args "\$@"
-EOL
-# Content that will serve as the launching script for citra (within the .app folder)
-
 # Make the launching script executable
 chmod +x ${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt
 
