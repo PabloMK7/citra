834a4873885ae84b7118bac1f05837bdfb549091
Jhynjhiruu <cub3r12@gmail.com>
2019-07-21 10:51:59 +0100

    Fix build issue
    Co-Authored-By: Ben <bene_thomas@web.de>

--- src/core/loader/smdh.cpp
+++ src/core/loader/smdh.cpp
@@ -53,7 +53,8 @@ SMDH::GameRegion SMDH::GetRegion() const {
         return GameRegion::RegionFree;
     }
 
-    constexpr u32 taiwan_and_china = (1 << GameRegion::Taiwan) & (1 << GameRegion::China);
+    constexpr u32 taiwan_and_china =
+        (1 << static_cast<u32>(GameRegion::Taiwan)) & (1 << static_cast<u32>(GameRegion::China));
     if (region_lockout == taiwan_and_china) {
         return GameRegion::Taiwan;
     } // hack to fix TWN games that support CHN consoles
