8d8bc8fee72deb8221f7f302630bc731b48a0ce0
vitor-k <vitor-kiguchi@hotmail.com>
2019-09-18 15:26:38 -0300

    small changes to ApplicationState handling, just in case

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -503,12 +503,15 @@ void GMainWindow::OnAppFocusStateChanged(Qt::ApplicationState state) {
     if (!UISettings::values.pause_when_in_background) {
         return;
     }
+    if (state != Qt::ApplicationHidden && state != Qt::ApplicationInactive &&
+        state != Qt::ApplicationActive) {
+        LOG_DEBUG(Frontend, "ApplicationState unusual flag: {} ", state);
+    }
     if (ui.action_Pause->isEnabled() &&
         (state & (Qt::ApplicationHidden | Qt::ApplicationInactive))) {
         auto_paused = true;
         OnPauseGame();
-    }
-    if (ui.action_Start->isEnabled() && auto_paused && state == Qt::ApplicationActive) {
+    } else if (ui.action_Start->isEnabled() && auto_paused && state == Qt::ApplicationActive) {
         auto_paused = false;
         OnStartGame();
     }
