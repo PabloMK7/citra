29c12058c755d3fb5b3d40722c9846a0f1bfb2e5
James Rowe <jroweboy@gmail.com>
2019-09-21 23:56:50 -0600

    Add minimum window size back to citra-qt

--- src/citra_qt/bootmanager.cpp
+++ src/citra_qt/bootmanager.cpp
@@ -256,6 +256,7 @@ void GRenderWindow::InitRenderTarget() {
     // WA_DontShowOnScreen, WA_DeleteOnClose
     core_context = CreateSharedContext();
     resize(Core::kScreenTopWidth, Core::kScreenTopHeight + Core::kScreenBottomHeight);
+    OnMinimalClientAreaChangeRequest(GetActiveConfig().min_client_area_size);
     BackupGeometry();
 }
 
