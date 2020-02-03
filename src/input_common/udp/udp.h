b39a611a3d79912476161da4efffa259c2dd0223
Lioncash <mathew1800@gmail.com>
2020-02-03 09:09:06 -0500

    input_common/udp: Add missing header guard

--- src/input_common/udp/udp.h
+++ src/input_common/udp/udp.h
@@ -2,6 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#pragma once
+
 #include <memory>
 #include <unordered_map>
 #include "input_common/main.h"
