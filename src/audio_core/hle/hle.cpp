3410b96400f1aaaa5d20937279f98833c5e65f09
zhang wei <zwdreams@gmail.com>
2020-03-17 10:07:22 +0800

    mediandk for android (#4921)

--- src/audio_core/hle/hle.cpp
+++ src/audio_core/hle/hle.cpp
@@ -7,6 +7,8 @@
 #include "audio_core/hle/wmf_decoder.h"
 #elif HAVE_FFMPEG
 #include "audio_core/hle/ffmpeg_decoder.h"
+#elif ANDROID
+#include "audio_core/hle/mediandk_decoder.h"
 #elif HAVE_FDK
 #include "audio_core/hle/fdk_decoder.h"
 #endif
@@ -99,6 +101,8 @@ DspHle::Impl::Impl(DspHle& parent_, Memory::MemorySystem& memory) : parent(paren
     decoder = std::make_unique<HLE::WMFDecoder>(memory);
 #elif defined(HAVE_FFMPEG)
     decoder = std::make_unique<HLE::FFMPEGDecoder>(memory);
+#elif ANDROID
+    decoder = std::make_unique<HLE::MediaNDKDecoder>(memory);
 #elif defined(HAVE_FDK)
     decoder = std::make_unique<HLE::FDKDecoder>(memory);
 #else
