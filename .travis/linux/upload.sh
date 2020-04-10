701cd8378623ea99b21c18e5eed8519fe9b78f4a
Hamish Milne <hamishmilne83@gmail.com>
2020-04-10 20:20:54 +0100

    Attempt to fix mac and linux builds

--- .travis/linux/upload.sh
+++ .travis/linux/upload.sh
@@ -8,9 +8,9 @@ COMPRESSION_FLAGS="-cJvf"
 
 mkdir "$REV_NAME"
 
-cp build/bin/citra "$REV_NAME"
-cp build/bin/citra-room "$REV_NAME"
-cp build/bin/citra-qt "$REV_NAME"
+cp build/bin/Release/citra "$REV_NAME"
+cp build/bin/Release/citra-room "$REV_NAME"
+cp build/bin/Release/citra-qt "$REV_NAME"
 
 # We need icons on Linux for .desktop entries
 mkdir "$REV_NAME/dist"
