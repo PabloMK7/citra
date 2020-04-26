93453dddb4bc300cf9caa3ee92ee522b6a11d33b
B3n30 <benediktthomas@gmail.com>
2020-04-26 10:43:39 +0200

    test: use gold instead of ld

--- .travis/linux-frozen/build.sh
+++ .travis/linux-frozen/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -ex
 mkdir -p "$HOME/.ccache"
-docker pull citraemu/build-environments:linux-frozen
 docker run --env-file .travis/common/travis-ci.env -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache citraemu/build-environments:linux-frozen /bin/bash -ex /citra/.travis/linux-frozen/docker.sh
