2e3834f880d6c3ebeea24e2374e2beb2a471a2e6
Vitor K <vitor-kiguchi@hotmail.com>
2021-02-07 05:40:10 -0300

    Fix macos build (#5703)
    * Update deps.sh
    
    GitHub's virtual environment provides 7zip and llvm by default, so brew may fail to install them as another version is already installed
    
    * Use || true to ignore brew installation errors

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -3,5 +3,5 @@
 brew update
 brew unlink python@2 || true
 rm '/usr/local/bin/2to3' || true
-brew install qt5 sdl2 p7zip ccache ffmpeg llvm ninja
+brew install qt5 sdl2 p7zip ccache ffmpeg llvm ninja || true
 pip3 install macpack
