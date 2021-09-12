154181d5f5c0cc8a1e310f9fcd63fa74b337de02
SachinVin <sachinvinayak2000@gmail.com>
2021-09-13 00:57:29 +0530

    CI/MacOS: Install SDL2 and FFMPEG from externals

--- .ci/macos/build.sh
+++ .ci/macos/build.sh
@@ -3,20 +3,25 @@
 set -o pipefail
 
 export Qt5_DIR=$(brew --prefix)/opt/qt5
-export PATH="/usr/local/opt/ccache/libexec:/usr/local/opt/llvm/bin:$PATH"
+export PATH="/usr/local/opt/ccache/libexec:$PATH"
 # ccache configurations
 export CCACHE_CPP2=yes
 export CCACHE_SLOPPINESS=time_macros
 
 export CC="ccache clang"
 export CXX="ccache clang++"
-export LDFLAGS="-L/usr/local/opt/llvm/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
 
 ccache -s
 
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DENABLE_QT_TRANSLATION=ON -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DUSE_DISCORD_PRESENCE=ON -DENABLE_FFMPEG_AUDIO_DECODER=ON -DENABLE_FFMPEG_VIDEO_DUMPER=ON -GNinja
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_QT_TRANSLATION=ON \
+    -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} \
+    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
+    -DUSE_DISCORD_PRESENCE=ON \
+    -DENABLE_FFMPEG_AUDIO_DECODER=ON \
+    -DENABLE_FFMPEG_VIDEO_DUMPER=ON \
+    -GNinja
 ninja
 
 ccache -s
