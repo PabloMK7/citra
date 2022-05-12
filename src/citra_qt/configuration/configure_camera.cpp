0d955c452bb8a76fe0e68a04fe7e779b646e768f
Morph <39850852+Morph1984@users.noreply.github.com>
2022-05-12 00:23:57 -0400

    citra_qt: Resolve C4267 warning on MSVC

--- src/citra_qt/configuration/configure_camera.cpp
+++ src/citra_qt/configuration/configure_camera.cpp
@@ -256,7 +256,7 @@ void ConfigureCamera::SetConfiguration() {
     int index = GetSelectedCameraIndex();
     for (std::size_t i = 0; i < Implementations.size(); i++) {
         if (Implementations[i] == camera_name[index]) {
-            ui->image_source->setCurrentIndex(i);
+            ui->image_source->setCurrentIndex(static_cast<int>(i));
         }
     }
     if (camera_name[index] == "image") {
