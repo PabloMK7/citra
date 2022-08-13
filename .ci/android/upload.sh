ddc802d2b4023324d3d5d1f896417d800034fef5
SachinVin <sachinvinayak2000@gmail.com>
2022-08-13 21:22:15 +0530

    android: add canary and nightly flavours + fix git version tags

--- .ci/android/upload.sh
+++ .ci/android/upload.sh
@@ -3,8 +3,11 @@
 . ./.ci/common/pre-upload.sh
 
 REV_NAME="citra-${GITDATE}-${GITREV}"
+[ "${GITHUB_REPOSITORY}" = "citra-emu/citra-canary" ] &&
+   BUILD_FLAVOR=canary ||
+   BUILD_FLAVOR=nightly
 
-cp src/android/app/build/outputs/apk/release/app-release.apk \
+cp src/android/app/build/outputs/apk/${BUILD_FLAVOR}/release/app-${BUILD_FLAVOR}-release.apk \
   "artifacts/${REV_NAME}.apk"
-cp src/android/app/build/outputs/bundle/release/app-release.aab \
+cp src/android/app/build/outputs/bundle/${BUILD_FLAVOR}Release/app-${BUILD_FLAVOR}-release.aab \
   "artifacts/${REV_NAME}.aab"
