ccb50e7f2c5d256d88564ecbca1c4c90e7f99cfb
Tobias <thm.frey@gmail.com>
2022-12-17 16:06:38 +0100

    Port yuzu-emu/yuzu#9300: "CMake: Use precompiled headers to improve compile times" (#6213)
    Co-authored-by: Ameer J <52414509+ameerj@users.noreply.github.com>

---
+++ src/common/common_precompiled_headers.h
@@ -0,0 +1,15 @@
+// Copyright 2022 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <memory>
+
+#include <fmt/format.h>
+
+#include "common/assert.h"
+#include "common/common_types.h"
---
+++ src/common/precompiled_headers.h
@@ -0,0 +1,7 @@
+// Copyright 2022 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "common/common_precompiled_headers.h"
