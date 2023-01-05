5d0e60a6aada7b3ff42700b1ed08da518f7d604b
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2023-01-05 15:16:40 -0800

    ci: Generate universal macOS build.

--- .ci/macos/deps.sh
+++ .ci/macos/deps.sh
@@ -1,23 +1,20 @@
 #!/bin/sh -ex
 
-brew update
-brew unlink python@2 || true
-rm '/usr/local/bin/2to3' || true
-brew install qt5 p7zip ccache ninja || true
+brew install ccache ninja || true
 pip3 install macpack
 
-export SDL_VER=2.0.16
-export FFMPEG_VER=4.4
+export FFMPEG_VER=5.1
+export QT_VER=5.15.8
 
 mkdir tmp
 cd tmp/
-
-# install SDL
-wget https://github.com/SachinVin/ext-macos-bin/raw/main/sdl2/sdl-${SDL_VER}.7z
-7z x sdl-${SDL_VER}.7z
-cp -rv $(pwd)/sdl-${SDL_VER}/* /
 
 # install FFMPEG
 wget https://github.com/SachinVin/ext-macos-bin/raw/main/ffmpeg/ffmpeg-${FFMPEG_VER}.7z
 7z x ffmpeg-${FFMPEG_VER}.7z
 cp -rv $(pwd)/ffmpeg-${FFMPEG_VER}/* /
+
+# install Qt
+wget https://github.com/SachinVin/ext-macos-bin/raw/main/qt/qt-${QT_VER}.7z
+7z x qt-${QT_VER}.7z
+sudo cp -rv $(pwd)/qt-${QT_VER}/* /usr/local/
