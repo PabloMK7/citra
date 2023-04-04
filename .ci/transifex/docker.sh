31cd8120b6956f7aa869eebf8002be10f9cb0e7f
GPUCode <47210458+GPUCode@users.noreply.github.com>
2023-04-05 01:26:58 +0300

    transifex: Remove python client (#6401)

--- .ci/transifex/docker.sh
+++ .ci/transifex/docker.sh
@@ -2,20 +2,6 @@
 
 set -x
 
-cat << 'EOF' > /usr/bin/tx
-#!/usr/bin/python3
-
-# -*- coding: utf-8 -*-
-import re
-import sys
-
-from txclib.cmdline import main
-
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
-EOF
-
 echo -e "\e[1m\e[33mBuild tools information:\e[0m"
 cmake --version
 gcc -v
