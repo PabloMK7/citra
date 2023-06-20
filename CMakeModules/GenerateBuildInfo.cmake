89663e0db82a1a246e4b133d64589f3f401a17ab
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2023-06-20 09:44:20 -0700

    build: Fix generating build info using unified source archive. (#6628)

--- CMakeModules/GenerateBuildInfo.cmake
+++ CMakeModules/GenerateBuildInfo.cmake
@@ -3,19 +3,32 @@ function(get_timestamp _var)
     string(TIMESTAMP timestamp UTC)
     set(${_var} "${timestamp}" PARENT_SCOPE)
 endfunction()
+get_timestamp(BUILD_DATE)
 
 list(APPEND CMAKE_MODULE_PATH "${SRC_DIR}/externals/cmake-modules")
 
-# Find the package here with the known path so that the GetGit commands can find it as well
-find_package(Git QUIET PATHS "${GIT_EXECUTABLE}")
+if (EXISTS "${SRC_DIR}/.git/objects")
+    # Find the package here with the known path so that the GetGit commands can find it as well
+    find_package(Git QUIET PATHS "${GIT_EXECUTABLE}")
 
-# generate git/build information
-include(GetGitRevisionDescription)
-get_git_head_revision(GIT_REF_SPEC GIT_REV)
+    # only use Git to check revision info when source is obtained via Git
+    include(GetGitRevisionDescription)
+    get_git_head_revision(GIT_REF_SPEC GIT_REV)
+    git_describe(GIT_DESC --always --long --dirty)
+    git_branch_name(GIT_BRANCH)
+elseif (EXISTS "${SRC_DIR}/GIT-COMMIT" AND EXISTS "${SRC_DIR}/GIT-TAG")
+    # unified source archive
+    file(READ "${SRC_DIR}/GIT-COMMIT" GIT_REV_RAW LIMIT 64)
+    string(STRIP "${GIT_REV_RAW}" GIT_REV)
+    string(SUBSTRING "${GIT_REV_RAW}" 0 9 GIT_DESC)
+    set(GIT_BRANCH "HEAD")
+else()
+    # self-packed archive?
+    set(GIT_REV "UNKNOWN")
+    set(GIT_DESC "UNKNOWN")
+    set(GIT_BRANCH "UNKNOWN")
+endif()
 string(SUBSTRING "${GIT_REV}" 0 7 GIT_SHORT_REV)
-git_describe(GIT_DESC --always --long --dirty)
-git_branch_name(GIT_BRANCH)
-get_timestamp(BUILD_DATE)
 
 # Generate cpp with Git revision from template
 # Also if this is a CI build, add the build name (ie: Nightly, Canary) to the scm_rev file as well
