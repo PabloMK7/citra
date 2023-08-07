a478bedb123e4d4e5d48b16dc07071e620bc7357
GPUCode <47210458+GPUCode@users.noreply.github.com>
2023-08-08 01:10:11 +0300

    rasterizer_cache: Bring back custom format parameter (#6873)

--- src/video_core/rasterizer_cache/surface_params.cpp
+++ src/video_core/rasterizer_cache/surface_params.cpp
@@ -228,10 +228,10 @@ std::string SurfaceParams::DebugName(bool scaled, bool custom) const noexcept {
 }
 
 bool SurfaceParams::operator==(const SurfaceParams& other) const noexcept {
-    return std::tie(addr, end, width, height, stride, levels, is_tiled, texture_type,
-                    pixel_format) == std::tie(other.addr, other.end, other.width, other.height,
-                                              other.stride, other.levels, other.is_tiled,
-                                              other.texture_type, other.pixel_format);
+    return std::tie(addr, end, width, height, stride, levels, is_tiled, texture_type, pixel_format,
+                    custom_format) ==
+           std::tie(other.addr, other.end, other.width, other.height, other.stride, other.levels,
+                    other.is_tiled, other.texture_type, other.pixel_format, other.custom_format);
 }
 
 } // namespace VideoCore
