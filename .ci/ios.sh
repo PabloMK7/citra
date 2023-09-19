af5202f6cb5f43c1f5d87a6f8b9f27a07fda2e1f
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2023-09-18 20:12:28 -0700

    ci: Add iOS verification workflow. (#6992)

---
+++ .ci/ios.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+mkdir build && cd build
+cmake .. -GNinja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_SYSTEM_NAME=iOS \
+    -DCMAKE_OSX_ARCHITECTURES=arm64 \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+    -DENABLE_QT_TRANSLATION=ON \
+    -DCITRA_ENABLE_COMPATIBILITY_REPORTING=ON \
+    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON
+ninja
+
+ccache -s
