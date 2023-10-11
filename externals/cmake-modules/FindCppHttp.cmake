775a25b49267240ad2dc0d8fcebbde3d0b454e05
Castor215 <132155746+Castor215@users.noreply.github.com>
2023-10-11 22:43:36 +0100

    externals: allow system cpp-cttplib to be used with both meson and cpp-httplib builds (#7062)
    Co-authored-by: Violet Purcell <vimproved@inventati.org>

--- externals/cmake-modules/FindCppHttp.cmake
+++ externals/cmake-modules/FindCppHttp.cmake
@@ -1,21 +1,38 @@
 if(NOT CppHttp_FOUND)
-    pkg_check_modules(HTTP_TMP cpp-httplib)
+    pkg_check_modules(CPP_HTTPLIB cpp-httplib)
 
-    find_path(CPP-HTTP_INCLUDE_DIR NAMES httplib.h
-            PATHS
-            ${HTTP_TMP_INCLUDE_DIRS}
-            /usr/include
-            /usr/local/include
-        )
-		
-    find_library(CPP-HTTP_LIBRARIES NAMES cpp-httplib
-            PATHS
-            ${HTTP_TMP_LIBRARY_DIRS}
-            /usr/lib
-            /usr/local/lib
-        )
+    if (CPP_HTTPLIB_FOUND)
+        find_path(HTTPLIB_INCLUDE_DIR NAMES httplib.h
+                PATHS
+                ${CPP_HTTPLIB_INCLUDE_DIRS}
+                /usr/include
+                /usr/local/include
+            )
 
-    find_package_handle_standard_args(CppHttp REQUIRED_VARS CPP-HTTP_INCLUDE_DIR CPP-HTTP_LIBRARIES VERSION_VAR HTTP_TMP_VERSION)
+        find_library(HTTPLIB_LIBRARY NAMES cpp-httplib
+                PATHS
+                ${CPP_HTTPLIB_LIBRARY_DIRS}
+                /usr/lib
+                /usr/local/lib
+            )
+
+        set(HTTPLIB_VERSION ${CPP_HTTPLIB_VERSION})
+
+        if (NOT TARGET cpp-httplib::cpp-httplib)
+            add_library(cpp-httplib::cpp-httplib INTERFACE IMPORTED)
+            set_target_properties(cpp-httplib::cpp-httplib PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${HTTPLIB_INCLUDE_DIR}"
+                INTERFACE_LINK_LIBRARIES "${HTTPLIB_LIBRARY}"
+                IMPORTED_LOCATION "${HTTPLIB_LIBRARY}"
+            )
+            add_library(httplib::httplib ALIAS cpp-httplib::cpp-httplib)
+        endif()
+    else()
+        message(STATUS "Cpp-httplib not found via pkg-config, trying CMake...")
+        find_package(httplib)
+    endif()
+
+    find_package_handle_standard_args(CppHttp REQUIRED_VARS HTTPLIB_INCLUDE_DIR HTTPLIB_LIBRARY VERSION_VAR HTTPLIB_VERSION)
 	
 endif()
 
