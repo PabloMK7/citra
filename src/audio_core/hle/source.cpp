9c84721d846d31afdff23865b475e85bcbfed2e4
SachinVin <26602104+SachinVin@users.noreply.github.com>
2024-01-15 01:57:28 +0530

    audio_core/hle/source.cpp: clear config.play_position_dirty regardless of config.play_position value. (#7349)
    Cosmetic-ish change so we dont incorrectly log about unhandled dirty flags

--- src/audio_core/hle/source.cpp
+++ src/audio_core/hle/source.cpp
@@ -161,12 +161,12 @@ void Source::ParseConfig(SourceConfiguration::Configuration& config,
                   static_cast<std::size_t>(state.mono_or_stereo));
     }
 
+    // play_position applies only to the embedded buffer, and defaults to 0 w/o a dirty bit
+    // This will be the starting sample for the first time the buffer is played.
     u32_dsp play_position = {};
-    if (config.play_position_dirty && config.play_position != 0) {
+    if (config.play_position_dirty) {
         config.play_position_dirty.Assign(0);
         play_position = config.play_position;
-        // play_position applies only to the embedded buffer, and defaults to 0 w/o a dirty bit
-        // This will be the starting sample for the first time the buffer is played.
     }
 
     // TODO(xperia64): Is this in the correct spot in terms of the bit handling order?
