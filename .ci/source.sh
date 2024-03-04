06b3bed49a2dff1e680ea9c847c09703c26518bf
PabloMK7 <hackyglitch2@gmail.com>
2024-03-04 22:35:41 +0100

    Update .gitmodules

--- .ci/source.sh
+++ .ci/source.sh
@@ -9,7 +9,7 @@ COMPAT_LIST='dist/compatibility_list/compatibility_list.json'
 mkdir artifacts
 
 pip3 install git-archive-all
-wget -q https://api.citra-emu.org/gamedb -O "${COMPAT_LIST}"
+touch "${COMPAT_LIST}"
 git describe --abbrev=0 --always HEAD > GIT-COMMIT
 git describe --tags HEAD > GIT-TAG || echo 'unknown' > GIT-TAG
 git archive-all --include "${COMPAT_LIST}" --include GIT-COMMIT --include GIT-TAG --force-submodules artifacts/"${REV_NAME}.tar"
