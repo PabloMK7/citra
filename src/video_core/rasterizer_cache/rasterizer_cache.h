6e3a01399b19e770a3a061047ae641d95d5072b3
PabloMK7 <hackyglitch2@gmail.com>
2024-06-14 14:48:28 +0200

    make variable const

--- src/video_core/rasterizer_cache/rasterizer_cache.h
+++ src/video_core/rasterizer_cache/rasterizer_cache.h
@@ -647,7 +647,7 @@ typename T::Surface& RasterizerCache<T>::GetTextureCube(const TextureCubeConfig&
 
     Surface& cube_surface = slot_surfaces[cube.surface_id];
     for (u32 i = 0; i < addresses.size(); i++) {
-        SurfaceId& face_id = cube.face_ids[i];
+        const SurfaceId& face_id = cube.face_ids[i];
         if (!addresses[i] || !face_id) {
             continue;
         }
