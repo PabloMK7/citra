ce33e808ff92d5a8c53bd25e33269b6b1ef2c9f4
PabloMK7 <hackyglitch2@gmail.com>
2024-07-16 12:47:33 +0200

    Revert "android: implement device rotation options (#182)"
    This reverts commit 93025c95f2ce4baed37a7b587236f12ef667c540.

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/IntSetting.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/IntSetting.kt
@@ -4,8 +4,6 @@
 
 package org.citra.citra_emu.features.settings.model
 
-import android.content.pm.ActivityInfo
-
 enum class IntSetting(
     override val key: String,
     override val section: String,
@@ -43,8 +41,7 @@ enum class IntSetting(
     DEBUG_RENDERER("renderer_debug", Settings.SECTION_DEBUG, 0),
     TEXTURE_FILTER("texture_filter", Settings.SECTION_RENDERER, 0),
     USE_FRAME_LIMIT("use_frame_limit", Settings.SECTION_RENDERER, 1),
-    DELAY_RENDER_THREAD_US("delay_game_render_thread_us", Settings.SECTION_RENDERER, 0),
-    DEVICE_ORIENTATION("default_device_orientation", Settings.SECTION_RENDERER, ActivityInfo.SCREEN_ORIENTATION_USER);
+    DELAY_RENDER_THREAD_US("delay_game_render_thread_us", Settings.SECTION_RENDERER, 0);
 
     override var int: Int = defaultValue
 
