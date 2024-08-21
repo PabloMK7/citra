97ca1096aad7e1569d849af01e6c4b8c050d865f
Daniel López Guimaraes <danielectra@outlook.com>
2024-08-21 10:55:54 +0100

    act: Add more command names and implement GetErrorCode
    The command names have been extracted from 3dbrew and the JavaScript
    bindings that the NNID settings uses internally.
    
    The error names have been extracted from the Wii U implementation
    (102-XXXX), which are compatible with the 3DS ones except for 022-5XXX,
    which are error codes specific to the 3DS.

--- src/core/hle/service/act/act_u.cpp
+++ src/core/hle/service/act/act_u.cpp
@@ -11,10 +11,31 @@ ACT_U::ACT_U(std::shared_ptr<Module> act) : Module::Interface(std::move(act), "a
     static const FunctionInfo functions[] = {
         // clang-format off
         {0x0001, &ACT_U::Initialize, "Initialize"},
-        {0x0002, nullptr, "GetErrorCode"},
-        {0x0006, &ACT_U::GetAccountDataBlock, "GetAccountDataBlock"},
+        {0x0002, &ACT_U::GetErrorCode, "GetErrorCode"},
+        {0x0003, nullptr, "GetLastResponseCode"},
+        {0x0005, nullptr, "GetCommonInfo"},
+        {0x0006, &ACT_U::GetAccountInfo, "GetAccountInfo"},
+        {0x0007, nullptr, "GetResultAsync"},
+        {0x0008, nullptr, "GetMiiImageData"},
+        {0x0009, nullptr, "SetNfsPassword"},
         {0x000B, nullptr, "AcquireEulaList"},
+        {0x000C, nullptr, "AcquireTimeZoneList"},
         {0x000D, nullptr, "GenerateUuid"},
+        {0x000F, nullptr, "FindSlotNoByUuid"},
+        {0x0010, nullptr, "SaveData"},
+        {0x0011, nullptr, "GetTransferableId"},
+        {0x0012, nullptr, "AcquireNexServiceToken"},
+        {0x0013, nullptr, "GetNexServiceToken"},
+        {0x0014, nullptr, "AcquireIndependentServiceToken"},
+        {0x0015, nullptr, "GetIndependentServiceToken"},
+        {0x0016, nullptr, "AcquireAccountInfo"},
+        {0x0017, nullptr, "AcquireAccountIdByPrincipalId"},
+        {0x0018, nullptr, "AcquirePrincipalIdByAccountId"},
+        {0x0019, nullptr, "AcquireMii"},
+        {0x001A, nullptr, "AcquireAccountInfoEx"},
+        {0x001D, nullptr, "InquireMailAddress"},
+        {0x001E, nullptr, "AcquireEula"},
+        {0x001F, nullptr, "AcquireEulaLanguageList"},
         // clang-format on
     };
     RegisterHandlers(functions);
