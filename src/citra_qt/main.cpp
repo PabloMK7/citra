d8760dee6aba49f5dce9a695e820316c59c70d07
kleidis <167202775+kleidis@users.noreply.github.com>
2024-08-21 19:59:07 +0200

    qt: Fix default language not being set to the main one the system uses (#236)
    * qt: Fix default language not being set to the main one the system uses
    
    * cleanup code

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -3093,8 +3093,8 @@ void GMainWindow::LoadTranslation() {
     bool loaded;
 
     if (UISettings::values.language.isEmpty()) {
-        // If the selected language is empty, use system locale
-        loaded = translator.load(QLocale(), {}, {}, QStringLiteral(":/languages/"));
+        // Use the system's default locale
+        loaded = translator.load(QLocale::system(), {}, {}, QStringLiteral(":/languages/"));
     } else {
         // Otherwise load from the specified file
         loaded = translator.load(UISettings::values.language, QStringLiteral(":/languages/"));
