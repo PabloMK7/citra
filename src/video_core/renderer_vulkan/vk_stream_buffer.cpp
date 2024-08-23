05e657414a1752dff788dd195625ad76c6256fcc
Jugurta <74503778+i0x404@users.noreply.github.com>
2024-08-23 19:12:55 +0100

    fix typo

--- src/video_core/renderer_vulkan/vk_stream_buffer.cpp
+++ src/video_core/renderer_vulkan/vk_stream_buffer.cpp
@@ -81,7 +81,7 @@ u32 GetMemoryType(const vk::PhysicalDeviceMemoryProperties& properties, BufferTy
 
     // If we reach here, we couldn't find any suitable memory type
     UNREACHABLE_MSG("Failed to find a suitable memory type for buffer type {}",
-+                    BufferTypeName(type));
+                    BufferTypeName(type));
 }
 
 constexpr u64 WATCHES_INITIAL_RESERVE = 0x4000;
